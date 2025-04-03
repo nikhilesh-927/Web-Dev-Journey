e765ec03cb4714ac623c382463549bdb094abfe5
Nikhilesh Mishra <mishrajinikhil8@gmail.com>
2025-04-03 18:35:28 +0530

    JavaScript completed

---
+++ 03_Javascript/DOM(Document Object Model)/01_DOM.js
@@ -0,0 +1,10 @@
+/* 
+Document Object Model: Document Object Model is an application
+programming interface (API) for manipulating HTML documents.
+
+The DOM provides functions that allow you to add, remove,
+and modify parts of the document effectively.
+
+The DOM represents an HTML document as a tree of nodes.
+
+*/
---
+++ 03_Javascript/DOM(Document Object Model)/02_Details_about_DOM(Document_Object_Model).js
@@ -0,0 +1,64 @@
+/* 
+
+JavaScript DOM (Document Object Model)
+
+1. What is DOM
+
+2. What is nodes and types of nodes
+
+3. Selecting elements using DOM
+
+    1.getElementById()
+    2.getElementsByName()
+    3.getElementsByTagName()
+    4.getElementsByClassName()
+    5.querySelector() and querySelectorAll()
+
+4. Traversing elements
+
+    1.selecting parent element
+    2.selecting child element
+    3.selecting sibling element
+
+5. Manipulating HTML elements
+
+    1.createElement()
+    2.appendChild()
+    3.textContent
+    4.innerHTML
+    5.after()
+    6.append()
+    7.prepend()
+    8.insertAdjacentHTML()
+    9.replaceChild()
+    10.cloneNode()
+    11.removeChild()
+    12.insertBefore()
+
+6. Attribute methods
+
+    1.getAttribute()
+    2.setAttribute()
+    3.hasAttribute()
+    4.removeAttribute()
+
+7. Manipulating Element's Styles
+
+    1.style property
+    2.cssText
+    3.getComputedStyle()
+    4.className property
+    5.classList property
+
+8. JavaScript Events
+
+    1.What is event
+    2.Event Bubbling & Event Capturing
+    3.Event Handler in HTML Attributes
+    4.DOM Level 0 event handlers
+    5.addEventListener()
+    6.removeEventListener()
+    7.Event Objects
+    8.Different Types of Events
+
+ */
